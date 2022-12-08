79ab750e99cd99c85a5fdf177b473f2d536bef31
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-08 20:08:43 +0100

    code refactoring

--- convertToMp3.py
+++ convertToMp3.py
@@ -1,32 +1,35 @@
+# Path: convertToMp3.py
 import os
+class CoverterToMp3:
+    def __init__(self, from_format, to_format, sorce_path, destination_path):
+        self.from_format = from_format
+        self.to_format = to_format
+        self.sorce_path = sorce_path
+        self.destination_path = destination_path
 
-# The path to the folder containing the .webm files
-webm_folder = os.path.join(r"C:\Users\ibrah\pythonScrips", "videos2")
+    def convert(self):
+        self.rename()
+        files = [f for f in os.listdir(
+            self.sorce_path) if f.endswith(self.from_format)]
+        for file in files:
+            input_path = os.path.join(self.sorce_path, file)
+            output_path = os.path.join(
+                self.destination_path, file.replace(self.from_format, self.to_format))
+            print(file)
+            os.system(
+                f"ffmpeg -i {input_path} -acodec libmp3lame {output_path}")
 
-# The path to the folder where the .mp3 files should be saved
-mp3_folder = os.path.join(r"C:\Users\ibrah\pythonScrips", "videos2", "mp3")
+    def rename(self):
+        files = [f for f in os.listdir(
+            self.destination_path) if f.endswith(self.to_format)]
+        for file in files:
+            input_path = os.path.join(self.destination_path, file)
+            output_path = os.path.join(self.destination_path, file.encode(
+                'ascii', 'ignore').decode('ascii').replace("_", "").replace("-", "").replace(" ", "_").replace("&", "_"))
+            os.rename(input_path, output_path)
 
-
-# Get a list of all .webm files in the webm folder
-webm_files = [f for f in os.listdir(webm_folder) if f.endswith(".webm")]
-
-for webm_file in webm_files:
-
-    # Construct the input and output file paths
-    input_path = os.path.join(webm_folder, webm_file)
-
-    output_path = os.path.join(webm_folder, webm_file.encode(
-        'ascii', 'ignore').decode('ascii').replace("_", "").replace("-", "").replace(" ", "_").replace("&", "_"))
-    os.rename(input_path, output_path)
-
-webm_files = [f for f in os.listdir(webm_folder) if f.endswith(".webm")]
-
-# Convert each .webm file to an .mp3 file
-
-for webm_file in webm_files:
-    # Construct the input and output file paths
-    input_path = os.path.join(webm_folder, webm_file)
-    output_path = os.path.join(mp3_folder, webm_file.replace(".webm", ".mp3"))
-    print(webm_file)
-    # Use ffmpeg to convert the .webm file to an .mp3 file
-    os.system(f"ffmpeg -i {input_path} -acodec libmp3lame {output_path}")
+    def list_files(self):
+        files = [f for f in os.listdir(
+            self.sorce_path) if f.endswith(self.from_format)]
+        for file in files:
+            print(file)
